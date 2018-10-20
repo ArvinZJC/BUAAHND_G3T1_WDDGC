a52996fc2f29ded4b20755fd422142f30014738a
ArvinZJC <tomzjc@qq.com>
2018-10-20 20:13:36 +0800

    some small changes

--- L3/L3_1/L3_1.js
+++ L3/L3_1/L3_1.js
@@ -1,7 +1,7 @@
 //2018.09.28, JavaScript code that is related to "L3_1.html"
 
 //control the "textTel" status according to the "radioStatus"
-function changeTextStatus()
+function ChangeTextStatus()
 {
     var radioStatus = document.getElementById( "enabled" );
     var textTel = document.getElementById( "tel" );
@@ -13,4 +13,4 @@ function changeTextStatus()
         textTel.value = "";
         textTel.disabled = "disabled";
     } //end if...else
-} //end function changeTextStatus
+} //end function ChangeTextStatus
---
+++ L4/L4_2/L4_2.js
@@ -0,0 +1,25 @@
+//2018.10.12, JavaScript code that is related to "L4_2.html"
+
+//get the current local time in real-time
+function getCurrentLocalTime()
+{
+    var time = new Date();
+    var year = time.getFullYear(), month = time.getMonth() + 1, date = time.getDate(), hour = time.getHours(), minute = time.getMinutes(), second = time.getSeconds();
+
+    month = ChangeTimeFormat( month ); //call the specified function to add a zero if "month" is less than 10
+    date = ChangeTimeFormat( date ); //call the specified function to add a zero if "date" is less than 10
+    hour = ChangeTimeFormat( hour ); //call the specified function to add a zero if "hour" is less than 10
+    minute = ChangeTimeFormat( minute ); //call the specified function to add a zero if "minute" is less than 10
+    second = ChangeTimeFormat( second ); //call the specified function to add a zero if "second" is less than 10
+    document.getElementById( "currentLocalTime" ).innerHTML = "Current local time: " + year + "/" + month + "/" + date + "  " + hour + ":" + minute + ":" + second;
+    setTimeout( "getCurrentLocalTime()", 500 );
+} //end function getCurrentTime
+
+//add a zero if the value is less than 10
+function ChangeTimeFormat( value )
+{
+    if( value < 10 )
+        return "0" + value;
+    else
+        return value;
+} //end function UpdateTime
